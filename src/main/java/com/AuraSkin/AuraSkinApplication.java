7205e36f8003df9c693e730ea6513e5055281e3c
João Victor <joaovictor.zuca12@gmail.com>
2026-05-09 16:58:27 -0300

    Spring Boot inicializado

---
+++ src/main/java/com/AuraSkin/AuraSkinApplication.java
@@ -0,0 +1,13 @@
+package com.AuraSkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraSkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuraSkinApplication.class, args);
+	}
+
+}
