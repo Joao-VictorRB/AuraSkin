601b7ff6c7da0c365266b8b9dfc99c2e23f360a0
João Victor <joaovictor.zuca12@gmail.com>
2026-05-09 17:00:13 -0300

    HomeController criado para o gerenciamento de rotas

---
+++ src/main/java/com/AuraSkin/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.AuraSkin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "API da AuraSkin funcionando!";
+    }
+    
+}
